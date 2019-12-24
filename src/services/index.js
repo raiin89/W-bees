eaaa1f35f0c5f725e56698f53b6388e763631936
dsoftware763 <dsoftware763@gmail.com>
2019-12-24 13:31:48 +0530

    Added Email Notifications

--- src/services/index.js
+++ src/services/index.js
@@ -3,6 +3,8 @@ const categories = require('./categories/categories.service.js');
 const jobs = require('./jobs/jobs.service.js');
 const bids = require('./bids/bids.service.js');
 const userRoles = require('./user-roles/user-roles.service.js');
+const mailer = require('./mailer/mailer.service.js');
+const authmanagement = require('./authmanagement/authmanagement.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
@@ -10,4 +12,6 @@ module.exports = function (app) {
   app.configure(jobs);
   app.configure(bids);
   app.configure(userRoles);
+  app.configure(mailer);
+  app.configure(authmanagement);
 };
