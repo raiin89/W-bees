89bda021a315efe84d349a87eeb2f35e66543088
w-bees server <dsoftware763@gmail.com>
2020-01-02 16:20:30 +0000

    resolved issuewith messages model

--- src/models/messages.model.js
+++ src/models/messages.model.js
@@ -11,7 +11,7 @@ module.exports = function (app) {
       allowNull: false
     },
     status: {
-      type: DataTypes.ENUM['sent', 'received', 'read'],
+      type: DataTypes.ENUM('sent', 'received', 'read'),
       allowNull: false
     }
   }, {
